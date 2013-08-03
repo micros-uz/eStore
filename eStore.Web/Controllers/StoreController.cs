864cd7686f6bc586630ebf9ad2566622581c64e7
Artem <artemsv@gmail.com>
2013-08-03 16:55:41 +0500

    Added: The whole project

---
+++ eStore.Web/Controllers/StoreController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace eStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        public ActionResult Index()
+        {
+            var list = new List<string>()
+            {
+                "Romans",
+                "Fixions",
+                "Novells"
+            };
+
+            return View(list);
+        }
+    }
+}
