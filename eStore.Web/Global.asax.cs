09c89a19b9144bfb5dd05310874e55e7d07eb5dd
micros-uz <micros-uz@gmail.com>
2013-08-04 17:59:46 -0700

    Added: Interfaces project

--- eStore.Web/Global.asax.cs
+++ eStore.Web/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eStore.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -20,6 +21,8 @@ namespace eStore.Web
 
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ControllerBuilder.Current.SetControllerFactory(new ControllerFactory());
         }
     }
 }
---
+++ eStore.Web/Logic/ControllerFactory.cs
@@ -0,0 +1,45 @@
+﻿using eStore.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.SessionState;
+
+namespace eStore.Logic
+{
+    internal class ControllerFactory : IControllerFactory
+    {
+        #region IControllerFactory
+
+        IController IControllerFactory.CreateController(RequestContext requestContext, string controllerName)
+        {
+            IController res = null;
+
+            switch(controllerName)
+            {
+                case "Home":
+                    res = new HomeController();
+                    break;
+                case "Store":
+                    res = new StoreController();
+                    break;                    
+            }
+
+            return res;
+        }
+
+        SessionStateBehavior IControllerFactory.GetControllerSessionBehavior(RequestContext requestContext, string controllerName)
+        {
+            return SessionStateBehavior.Disabled;
+        }
+
+        void IControllerFactory.ReleaseController(IController controller)
+        {
+            
+        }
+
+        #endregion
+    }
+}
