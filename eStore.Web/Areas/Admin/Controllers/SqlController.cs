3317b6b598a20f3e3aeaa4f7533b965134d16d75
micros-uz <micros-uz@gmail.com>
2013-08-06 01:58:52 -0700

    Added: DataTable to WebGrid converting

--- eStore.Web/Areas/Admin/Controllers/SqlController.cs
+++ eStore.Web/Areas/Admin/Controllers/SqlController.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Dynamic;
 using System.Web.Mvc;
+using WebMatrix.Data;
 
 namespace eStore.Web.Areas.Admin.Controllers
 {
@@ -14,11 +15,17 @@ namespace eStore.Web.Areas.Admin.Controllers
     {
         public ActionResult Index(ExecSqlModel model)
         {
-            model.User = "sa";
-            model.Password = "dev1234";
-            model.Server = ".";
-            model.DataBase = "Orygin";
-            model.Query = "select * from PluginMapping";
+            //model.User = "sa";
+            //model.Password = "dev1234";
+            //model.Server = ".";
+            //model.DataBase = "Orygin";
+            //model.Query = "select * from PluginMapping";
+
+            model.Server = "058e3204-8872-4364-b041-a20f00dbd586.sqlserver.sequelizer.com";
+            model.DataBase = "db058e320488724364b041a20f00dbd586";
+            model.User = "uqdvuhwlzvfhhjgs";
+            model.Password = "8yqBAvd7s6kWPwGnjznFapt6NedrV4iZaf3WoDrBKjEZfgUEsahZiSRSesVwrUjw";
+            model.Query = "";
 
             return View(model);
         }
@@ -34,7 +41,7 @@ namespace eStore.Web.Areas.Admin.Controllers
                 UserID = "uqdvuhwlzvfhhjgs",
                 Password = "8yqBAvd7s6kWPwGnjznFapt6NedrV4iZaf3WoDrBKjEZfgUEsahZiSRSesVwrUjw",
             }.ConnectionString;
-
+            //Server=058e3204-8872-4364-b041-a20f00dbd586.sqlserver.sequelizer.com;Database=db058e320488724364b041a20f00dbd586;User ID=uqdvuhwlzvfhhjgs;Password=8yqBAvd7s6kWPwGnjznFapt6NedrV4iZaf3WoDrBKjEZfgUEsahZiSRSesVwrUjw;
             var conn = new SqlConnection(connectionString);
 
             try
@@ -78,38 +85,40 @@ namespace eStore.Web.Areas.Admin.Controllers
                 conn.Open();
                 var cmd = new SqlCommand(model.Query, conn);
                 var reader = cmd.ExecuteReader();
-                //DataTable dt = new DataTable();
-                //dt.Load(reader);
 
                 if (reader.HasRows)
                 {
-                    var t = reader.GetSchemaTable();
+                    DataTable dt = new DataTable();
+                    dt.Load(reader);
 
-                    var cols = new List<string>();
-
-                    for (int i = 0; i < 1; i++)
+                    if (dt.Rows.Count > 0)
                     {
-                        cols.Add(t.Rows[i][0].ToString());
+                        var result = new List<dynamic>();
+                        foreach (DataRow row in dt.Rows)
+                        {
+                            var obj = (IDictionary<string, object>)new ExpandoObject();
+                            foreach (DataColumn col in dt.Columns)
+                            {
+                                obj.Add(col.ColumnName, row[col.ColumnName]);
+                            }
+                            result.Add(obj);
+                        }
+
+                        res.Data = result;
                     }
+                }
+                else if (reader.RecordsAffected > -1)
+                {
+                    var obj = (IDictionary<string, object>)new ExpandoObject();
+                    obj.Add("Row(s) affected", reader.RecordsAffected);
 
-                    res.Columns = cols;
-                    res.Headers = new List<string>() { "FF" };
-
-                    var data = new List<dynamic>();
-                    
-                    while (reader.Read())
-                    {
-                        var row = new { Id = reader.GetInt32(0) };
-                        data.Add(row);
-                    }
-
-                    res.Data = data;
+                    res.Data = new List<dynamic>() { obj };
                 }
             }
             catch(Exception ex)
             {
                 model.Result = ex.Message;
-                RedirectToAction("Index", model);
+                return RedirectToAction("Index", model);
             }
             finally
             {
