93aa8f47d3d0128a37e53956301f7dc1378248c2
micros-uz <micros-uz@gmail.com>
2013-09-01 02:03:26 +0500

    Added: Modal dialog for new genre

---
+++ eStore.Web.UI/Scripts/chooseGenre.js
@@ -0,0 +1,36 @@
+﻿$(function () {
+    $('#genreDialog').dialog({
+        autoOpen: false,
+        width: 400,
+        height: 300,
+        modal: true,
+        title: 'Add Genre',
+        buttons: {
+            'Save': function () {
+                var createGenreForm = $('#createGenreForm');
+                if (createGenreForm.valid()) {
+                    $.post(createGenreForm.attr('action'), createGenreForm.serialize(), function (data) {
+                        if (data.Error != '') {
+                            alert(data.Error);
+                        }
+                        else {
+                            // Add the new genre to the dropdown list and select it
+
+                            $('#GenreId').append(
+                                    $('<option></option>')
+                                        .val(data.Genre.GenreId)
+                                        .html(data.Genre.Name)
+                                        .prop('selected', true)  // Selects the new Genre in the DropDown LB
+                                );
+                            $('#genreDialog').dialog('close');
+                        }
+                    });
+                }
+            },
+
+            'Cancel': function () {
+                $(this).dialog('close');
+            }
+        }
+    });
+})
