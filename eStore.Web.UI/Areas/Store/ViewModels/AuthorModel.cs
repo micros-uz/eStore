330f55a18f0f4687c1930d82fba87a5d7a2eef8c
Artem <artemsv@gmail.com>
2013-09-03 21:26:12 +0500

    Added: new author modal

---
+++ eStore.Web.UI/Areas/Store/ViewModels/AuthorModel.cs
@@ -0,0 +1,17 @@
+﻿namespace eStore.Web.UI.Areas.Store.ViewModels
+{
+    public class AuthorModel
+    {
+        public int AuthorId
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+}
--- eStore.Web.UI/Scripts/eStore-chooseGenre.js
+++ eStore.Web.UI/Scripts/eStore-chooseGenre.js
@@ -1,18 +1,48 @@
-﻿//  THIS IS NOT CALLED???
-$('.modal').on('hidden', function () {
-    $(this).removeData();
-});
-//  THIS IS NOT CALLED TOO - WHY???
-$('#addGenre').on('hidden', function () {
-    $(this).removeData('modal');
-    alert('eee');
+﻿"use strict";
+
+$('.modal').on('hidden.bs.modal', function () {
+    $('input:text', this).val('');
 });
 
-function save() {
+$(document).ready(function () {
+    $(".modal").on('shown.bs.modal', function () {
+        $('input:text:visible:first', this).focus();     
+    });
+});
+
+function saveNewItem(model, url2, id, valProp, textProp) {
+    $.ajax({
+        type: "POST",
+        url: url2 + "/add",
+        data: JSON.stringify(model),
+        contentType: "application/json;charset=utf-8",
+        success: function () {
+            $.getJSON(url2, null, function (data) {
+                var ddl = $(id);
+                ddl.empty();
+
+                $(data).each(function (index, genre) {
+                    ddl.append($("<option />").val(model[valProp]).text(model[textProp]));
+                });
+                $(ddl).find('option:contains(' + model[textProp] + ')').attr('selected', 'selected');
+                $(id).modal('hide');
+            });
+        },
+        error: function (data) {
+            alert(data.Message);
+        }
+    });
+}
+
+function saveNewGenre() {
     var genreModel = {
         Title: $('#genreTitle').val(),
         Desc: $('#genreDesc').val()
     };
+
+    var ee = genreModel["Title"];
+
+//    saveNewItem(genreModel, "/api/genre", "GenreId", "Title");
 
     $.ajax({
         type: "POST",
