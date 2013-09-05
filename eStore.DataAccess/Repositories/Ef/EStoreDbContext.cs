826e9d932f74810113c0cb6e40cb547919f440a3
micros-uz <artemsv@gmail.com>
2013-09-05 21:08:52 +0500

    Added: Lost file

---
+++ eStore.DataAccess/Repositories/Ef/EStoreDbContext.cs
@@ -0,0 +1,49 @@
+﻿using System.Data.Entity;
+using eStore.Domain;
+
+namespace eStore.DataAccess.Repositories.Ef
+{
+    internal class EStoreDbContext : DbContext
+    {
+        public EStoreDbContext(string connStr)
+            : base(connStr)
+        {
+        }
+
+        public DbSet<Genre> Genres
+        {
+            get;
+            set;
+        }
+
+        public DbSet<Author> Authors
+        {
+            get;
+            set;
+        }
+
+        public DbSet<Book> Books
+        {
+            get;
+            set;
+        }
+
+        public DbSet<Series> Series
+        {
+            get;
+            set;
+        }
+
+        public DbSet<User> Users
+        {
+            get;
+            set;
+        }
+
+        public DbSet<Role> Roles
+        {
+            get;
+            set;
+        }
+    }
+}
