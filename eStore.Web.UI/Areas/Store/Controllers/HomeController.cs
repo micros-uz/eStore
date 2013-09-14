d5f4bd42a3425ed04f3a89073bf8bd045a349297
Artem <mmm@mail.mail>
2013-09-14 20:41:57 +0500

    Added: BestSellers and New Books

--- eStore.Web.UI/Areas/Store/Controllers/HomeController.cs
+++ eStore.Web.UI/Areas/Store/Controllers/HomeController.cs
@@ -1,13 +1,43 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using eStore.Domain;
+using eStore.Interfaces.Services;
+using eStore.Web.Infrastructure.ObjectMapper;
+using eStore.Web.UI.Areas.Store.ViewModels;
 
 namespace eStore.Web.UI.Areas.Store.Controllers
 {
     [AllowAnonymous]
     public class HomeController : Controller
     {
+        private readonly IStoreService _service;
+        private readonly IObjectMapper _mapper;
+
+        public HomeController(IStoreService service, IObjectMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var books = _service.GetBooksByGenre(9);
+
+            var models = _mapper.Map<IEnumerable<Book>, IEnumerable<BookFullModel>>(books);
+
+            return View(new HomeModel
+                {
+                    BestSellers = new GenreBooksModel
+                    {
+                        Books = models,
+                        GenreId = 9
+                    },
+                    NewBooks = new GenreBooksModel
+                    {
+                        Books = models,
+                        GenreId = 9
+                    }
+                });
         }
     }
 }
---
+++ eStore.Web.UI/Areas/Store/ViewModels/HomeModel.cs
@@ -0,0 +1,18 @@
+﻿
+namespace eStore.Web.UI.Areas.Store.ViewModels
+{
+    public class HomeModel
+    {
+        public GenreBooksModel BestSellers
+        {
+            get;
+            set;
+        }
+
+        public GenreBooksModel NewBooks
+        {
+            get;
+            set;
+        }
+    }
+}
